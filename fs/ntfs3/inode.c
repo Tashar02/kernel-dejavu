e334ebba3c8a9d98fff181413148e45ed103d732
Konstantin Komarov <almaz.alexandrovich@paragon-software.com>
2022-10-07 13:57:28 +0300

    fs/ntfs3: Fixing work with sparse clusters
    Simplify logic in ntfs_extend_initialized_size, ntfs_sparse_cluster
    and ntfs_fallocate.
    
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>

--- fs/ntfs3/inode.c
+++ fs/ntfs3/inode.c
@@ -646,9 +646,17 @@ static noinline int ntfs_get_block_vbo(struct inode *inode, u64 vbo,
 			bh->b_size = block_size;
 			off = vbo & (PAGE_SIZE - 1);
 			set_bh_page(bh, page, off);
-			err = bh_read(bh, 0);
-			if (err < 0)
+
+			lock_buffer(bh);
+			bh->b_end_io = end_buffer_read_sync;
+			get_bh(bh);
+			submit_bh(REQ_OP_READ, bh);
+
+			wait_on_buffer(bh);
+			if (!buffer_uptodate(bh)) {
+				err = -EIO;
 				goto out;
+			}
 			zero_user_segment(page, off + voff, off + block_size);
 		}
 	}
